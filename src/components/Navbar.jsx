51cc58198da0a2deab5a3ba01556b90726a0072b
Karan Vasupalli <vkaran14210@gmail.com>
2024-05-05 23:46:46 +0530

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,25 @@
+import { Link } from 'react-router-dom';
 
-
-function Navbar(){
-    return(
-        <>
-            <div className="flex justify-around  h-16 bg-[#111827]  text-white  items-center">
-                <div className=' flex gap-3 items-center'>
-                    <img src="https://flowbite.com/docs/images/logo.svg" alt="" />
-                    <h1>GeekFoods</h1>
-                </div>
-                <div className='nav2'>
-                    <li className=" flex gap-7">
-                        <link to ="/">Home</link>
-                        <link to ="/quote">Quote</link>
-                        <link to ="">Restaurant</link>
-                        <link to ="">Foods</link>
-                        <link to ="">Contact</link>
-                    </li>
-                </div>
-                <div className="px-4 py-2 rounded-md bg-[#2563eb]">
-                    <button className="tex">Get Started</button>
-                </div>
+function Navbar() {
+    return (
+        <div className="flex justify-around h-16 bg-[#111827] text-white items-center">
+            <div className='flex gap-3 items-center'>
+                <img src="https://flowbite.com/docs/images/logo.svg" alt="" />
+                <h1>GeekFoods</h1>
             </div>
-
-        </>
+            <div className='nav2'>
+                <ul className="flex gap-7">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/quote">Quote</Link></li>
+                    <li><Link to="/">Restaurant</Link></li>
+                    <li><Link to="/">Foods</Link></li>
+                    <li><Link to="/">Contact</Link></li>
+                </ul>
+            </div>
+            <div className="px-4 py-2 rounded-md bg-[#2563eb]">
+                <button className="tex">Get Started</button>
+            </div>
+        </div>
     )
 }
 
